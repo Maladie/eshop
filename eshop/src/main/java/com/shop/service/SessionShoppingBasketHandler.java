f096a87a43014741bcd6a72ef062b8a7e13e40cd
Krzysztof <reviewalfa1@gmail.com>
2017-10-10 18:41:27 +0200

    added "add to basket" functionality

--- eshop/src/main/java/com/shop/service/SessionShoppingBasketHandler.java
+++ eshop/src/main/java/com/shop/service/SessionShoppingBasketHandler.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpSession;
 public class SessionShoppingBasketHandler {
 
     public static Basket retrieveBasket(HttpSession session){
-        if(session.getAttribute("basket") != null) {
+        if(session.getAttribute("basket") == null) {
             session.setAttribute("basket", new Basket());
         }
             return (Basket) session.getAttribute("basket");
