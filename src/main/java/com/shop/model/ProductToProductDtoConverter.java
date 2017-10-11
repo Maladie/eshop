92316e5f698d43d9970f043283437e76eb588bd9
Maladie <mariusz.maks.szymanski@gmail.com>
2017-10-11 18:14:19 +0200

    ProductDtoConverter

---
+++ src/main/java/com/shop/model/ProductToProductDtoConverter.java
@@ -0,0 +1,10 @@
+package com.shop.model;
+
+import com.shop.model.factory.impl.Product;
+
+public class ProductToProductDtoConverter {
+
+    public static ProductDto convert(Product product){
+        ProductDto productDto = new ProductDto();
+    }
+}
