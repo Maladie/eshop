a7a95c51d167eff24d090b64c9633cda1cbbe0a4
krzychu124 <krzychu124@gmail.com>
2017-10-11 20:37:07 +0200

    poprawka błędów...

--- src/main/java/com/shop/model/BasketDto.java
+++ src/main/java/com/shop/model/BasketDto.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class BasketDto {
 
     List<ProductDto> productDtoList;
-//    private Basket basket;
+    private Basket basket;
 
     public BasketDto(Basket basket){
         productDtoList = new ArrayList<>();
