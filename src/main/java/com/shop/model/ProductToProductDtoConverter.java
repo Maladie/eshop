5d9222a56aa4dfdd46d8c545dc34d902a1541776
Maladie <mariusz.maks.szymanski@gmail.com>
2017-10-11 19:02:10 +0200

    ProductDtoConverter

--- src/main/java/com/shop/model/ProductToProductDtoConverter.java
+++ src/main/java/com/shop/model/ProductToProductDtoConverter.java
@@ -4,7 +4,9 @@ import com.shop.model.factory.impl.Product;
 
 public class ProductToProductDtoConverter {
 
-    public static ProductDto convert(Product product){
+    public static ProductDto convert(Product product) {
         ProductDto productDto = new ProductDto();
+        productDto.setId(product.getId());
+        productDto.setName();
     }
 }
