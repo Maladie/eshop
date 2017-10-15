3ee2dbf45194fd05e9f6394f4c10dc038361d5da
krzychu124 <krzychu124@gmail.com>
2017-10-15 22:06:15 +0200

    removed unused imports

--- src/main/java/com/shop/controller/AddToBasketController.java
+++ src/main/java/com/shop/controller/AddToBasketController.java
@@ -1,12 +1,6 @@
 package com.shop.controller;
 
-import com.shop.model.Basket;
-import com.shop.model.BasketDto;
-import com.shop.model.factory.impl.Product;
-import com.shop.repository.impl.HibernateRepositoryImpl;
-import com.shop.repository.impl.ProductRepositoryImpl;
 import com.shop.service.ProductService;
-import com.shop.service.SessionShoppingBasketHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
--- src/main/java/com/shop/controller/LoginController.java
+++ src/main/java/com/shop/controller/LoginController.java
@@ -8,10 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.time.LocalDateTime;
 
 @WebServlet(name = "LoginController", value = "/login")
 public class LoginController extends HttpServlet{
