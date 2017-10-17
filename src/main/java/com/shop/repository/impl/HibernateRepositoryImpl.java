f2b78079ffa128b8d01a424969e91fdcb3204ac2
krzychu124 <krzychu124@gmail.com>
2017-10-17 02:33:47 +0200

    fixed getProductBySearchCriteria query

--- src/main/java/com/shop/repository/impl/HibernateRepositoryImpl.java
+++ src/main/java/com/shop/repository/impl/HibernateRepositoryImpl.java
@@ -35,7 +35,7 @@ public class HibernateRepositoryImpl implements ProductRepository {
 
     @Override
     public List<Product> getProductsBySearchCritieria(String searchCriteria) {
-        return entityManager.createQuery("SELECT p from Product p WHERE p.name LIKE :search", Product.class).setParameter("search", searchCriteria).getResultList();
+        return entityManager.createQuery("SELECT p from Product p WHERE p.name LIKE :search", Product.class).setParameter("search", searchCriteria +"%").getResultList();
     }
 
     @Override
