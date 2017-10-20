32f2ee17c2ba315fa9dfe44467b66c93da57c504
krzychu124 <krzychu124@gmail.com>
2017-10-20 06:07:15 +0200

    added response to loginAvailability check request

--- src/main/java/com/shop/controller/RegistrationController.java
+++ src/main/java/com/shop/controller/RegistrationController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @WebServlet(name = "Registration", value = "/register")
 public class RegistrationController extends HttpServlet {
@@ -19,7 +20,15 @@ public class RegistrationController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        RegisterUserService.userService().registerUser(request);
-        response.sendRedirect("/home");
+        String validateUserName = request.getParameter("validateUserName");
+        if(validateUserName != null){
+            boolean result= RegisterUserService.userService().isLoginAvailable(validateUserName);
+            response.setContentType("text/plain");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(result? "available" : "unavailable");
+        } else {
+            RegisterUserService.userService().registerUser(request);
+            response.sendRedirect("/home");
+        }
     }
 }
